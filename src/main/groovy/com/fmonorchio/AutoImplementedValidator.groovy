f481be1145c9491796d1086181592f28cabc9424
fmonorchio <francesco@faire.ai>
2022-06-23 22:25:59 +0200

    Add mapstruct

---
+++ src/main/groovy/com/fmonorchio/AutoImplementedValidator.groovy
@@ -0,0 +1,8 @@
+package com.fmonorchio
+
+import groovy.transform.AutoImplement
+
+@AutoImplement
+class AutoImplementedValidator implements Validator {
+
+}
---
+++ src/main/groovy/com/fmonorchio/Validator.groovy
@@ -0,0 +1,7 @@
+package com.fmonorchio
+
+interface Validator {
+
+    boolean isValid()
+
+}
